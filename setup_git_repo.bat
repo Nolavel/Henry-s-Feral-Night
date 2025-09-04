e06c88f9da4279deaa2ef92b4812316787c17318
Nolavel <bobrusovsv78@gmail.com>
2025-09-04 09:05:09 +0800

    Cleanup: remove temporary files and bat scripts from repo

--- setup_git_repo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Initializing Git repository...
-git init
-echo.
-echo Adding remote repository...
-git remote add origin https://github.com/Nolavel/Crimson-Flow-or-Last-Man-Breathing
-echo.
-echo Setup complete. You can now use "git push" to upload your project.
-pause
